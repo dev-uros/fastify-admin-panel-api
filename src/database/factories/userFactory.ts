72022da1f39eace20d16e40ae946f52b6fe50454
dev-uros <uros.minic@kentkart.rs>
2024-03-07 16:36:39 +0100

    Adding process exit

--- src/database/factories/userFactory.ts
+++ src/database/factories/userFactory.ts
@@ -42,6 +42,7 @@ export default async function generateUser(numberOfUsersForCreation: number = 1)
                 .returningAll()
                 .executeTakeFirst();
 
+
             if(dbUser){
                 usersCreated++
 
--- src/database/seeders/databaseSeeder.ts
+++ src/database/seeders/databaseSeeder.ts
@@ -1,3 +1,7 @@
 import generateUser from "../factories/userFactory";
 
 await generateUser(10);
+
+console.log('database seeder executed');
+
+process.exit();
