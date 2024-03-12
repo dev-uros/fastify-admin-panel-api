58e063c1c2a35ddb2cc63933dc9cf91448017ec1
dev-uros <uros.minic@kentkart.rs>
2024-03-12 15:29:06 +0100

    Setting specific folders for user profile pictures

--- src/plugins/static.ts
+++ src/plugins/static.ts
@@ -7,8 +7,8 @@ export default fp(
         const __dirname = path.dirname(new URL(import.meta.url).pathname)
 
         fastify.register(fastifyStatic, {
-            root: path.join(__dirname, '..','public'),
-            prefix: '/public/',
+            root: path.join(__dirname, '..','public', 'user-profile-pictures'),
+                prefix: '/public/user-profile-pictures/',
             constraints: { host: `${fastify.config.HOST}:${fastify.config.PORT}` }
         })
     },
