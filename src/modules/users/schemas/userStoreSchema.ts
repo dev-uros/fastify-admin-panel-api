bef775ecec2da5f1ce87940f72fd8b67d17ca70d
dev-uros <uros.minic@kentkart.rs>
2024-03-13 16:41:52 +0100

    Refactor

--- src/modules/users/schemas/userStoreSchema.ts
+++ src/modules/users/schemas/userStoreSchema.ts
@@ -58,14 +58,7 @@ export type UserStoreRequestSchemaType = Static<typeof userStoreRequestSchema>
 export const userStoreResponseSchema = Type.Object(
     {
       message: Type.String(),
-      data: Type.Object({
-          id: Type.Number(),
-          created_at: Type.String({ format: 'date' }) as TString | TDate, // Use 'date-time' format instead of 'date'
-          first_name: Type.String(),
-          last_name: Type.Union([Type.String(), Type.Null()]),
-          email: Type.String({ format: 'email' }), // Add format validation for email
-          profile_picture_path: Type.String()
-      })
+      data: UserEntitySchema
     },
     {
       additionalProperties: false
